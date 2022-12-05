a1bded2724e16f8bc1f21ac9e16f21521989467b
Renat <104959479+RenatKhrashchevskyi@users.noreply.github.com>
2022-12-05 15:07:37 -0700

    Add files via upload

---
+++ battleship.js
@@ -0,0 +1,47 @@
+'use strict'
+
+
+function name(params) {
+	// // let randomLoc = Math.floor(Math.random() * 5);
+	// // let loc1 = randomLoc;
+	// // let loc2 = loc1 + 1;
+	// // let loc3 = loc2 + 1;
+	
+	// let loc1 = 3;
+	// let loc2 = 4;
+	// let loc3 = 5;
+	
+	// let guess;
+	// let hits = 0;
+	// let guesses = 0;
+	// let isSunk = false;
+	
+	// while (isSunk === false) {
+	//     guess = prompt('Ready, aim, fire!(enter a number 0-6):');
+	//     if (guess < 0 || guess > 6) {
+	//         alert('Please, enter a valid cell number!');
+	//     } else {
+	//         guesses += 1;
+	//     9
+	//        if (guess == loc1 || guess == loc2 || guess == loc3) {
+	//             hits += 1;
+	//             alert('HIT!');
+	        
+	//             if (hits == 3) {
+	//                 isSunk = true;
+	//                 alert('You sank my battleship!');
+	//             }
+	//       } else {
+	//         alert('MISS');
+	//     }
+	//     }  
+	// };
+	
+	// let stats = 'You took ' + guesses + ' guesses to sink the battleship, ' + 'which means your accuracy was ' + (3/guesses);
+	
+	// alert(stats);
+}
+
+
+
+
